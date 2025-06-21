9d5ebfe8dd47e8e03de6dfc1d3e34b0be6f7d65d
HaithamGhaith <haithammghaith@gmail.com>
2025-06-21 22:45:29 +0300

    Hid Firebase Key

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCMc039S4o_DUxgt4iKjjFu5sa4NZishX0",
-  authDomain: "ipbeep.firebaseapp.com",
-  projectId: "ipbeep",
-  storageBucket: "ipbeep.firebasestorage.app",
-  messagingSenderId: "430401935725",
-  appId: "1:430401935725:web:ef4af7ecbee326bb6ae264",
-  measurementId: "G-1NXLCLE1EW"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
